0ed7d97c5d27bfdfbbb17e55d3e1cbdc841bdb3c
vdatasci <joshvoss90@outlook.com>
2017-07-03 07:42:42 -0400

    Update unix_period.py

--- unix_period.py
+++ unix_period.py
@@ -5,14 +5,25 @@ import time
 #Press CTRL C to interrupt the unix_countup function:
 ###If highlight command line, then the function pauses but does not resync time
 def unix_countup(party=1500000000, moment=0.01, offset=0):
-    '''Unix Time Stamp CountUp'''
+    '''Unix Time Stamp Counter.
+    
+    unix_countup(party, moment, offset)
+
+    party:= counter end time
+    moment:= distinct measures of time
+    offset:= displacement from generated time
+    
+
+    Example:
+    unix_countup(party=1500000000, moment=0.01, offset=0)
+
+    '''
     now = round((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds() + offset, 2)
     while now < party:
         print("{:,}".format(now)),'\r',
         sys.stdout.flush()
         time.sleep(moment)
         now = round((datetime.datetime.utcnow() - datetime.datetime(1970, 1, 1)).total_seconds() + offset, 2)
-
 
 
 
