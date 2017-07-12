2798946b7e34628df44b32e3c665e4ab953cdc3d
vdatasci <joshvoss90@outlook.com>
2017-07-12 16:43:10 -0400

    Update unix_period.py

--- unix_period.py
+++ unix_period.py
@@ -1,7 +1,7 @@
 import time
 
 #Press CTRL C to interrupt the unix_countup function:
-def unix_countup(party=1500000000, msg='Party Time!'):
+def unix_countup(party=1500000000, *msg='Party Time!'):
     '''Unix Time Stamp Counter.
     
     unix_countup(party)
@@ -11,20 +11,23 @@ def unix_countup(party=1500000000, msg='Party Time!'):
     unix_countup(party=1600000000)
     '''
     
-    while True:
-        if time.time()<party:
-            print("{:,}".format(time.time()))
-            wait_time = 1-time.time()%1
-            time.sleep(wait_time if wait_time else 1)
-        else:
-            break
+    if msg:
+        while True:
+            if time.time()<party:
+                print("{:,}".format(time.time()))
+                wait_time = 1-time.time()%1
+                time.sleep(wait_time if wait_time else 1)
+            else:
+                print(msg)
     
-    if msg == True:
-        print(msg)
     else:
-        print("{:,}".format(time.time()))
-        wait_time = 1-time.time()%1
-        time.sleep(wait_time if wait_time else 1)
+        while True:
+            if time.time()<party:
+                print("{:,}".format(time.time()))
+                wait_time = 1-time.time()%1
+                time.sleep(wait_time if wait_time else 1)
+            else:
+                break
 
 
 
