c57296872355184790b47792f6fe7c662786ab9a
vdatasci <joshvoss90@outlook.com>
2017-07-12 16:22:25 -0400

    Update unix_counter.py

--- unix_counter.py
+++ unix_counter.py
@@ -23,19 +23,19 @@ Unix time (also known as POSIX time or epoch time) is a system for describing in
 COMMAND-LINE EXAMPLE (1):
  F:
  cd F:\\Python\\Projects\\Epoch
- python unix_counter.py 1500000000 0.01 0
+ python unix_counter.py 1500000000
 
 
 COMMAND-LINE EXAMPLE (2):
  F:
  cd F:\\Python\\Projects\\Epoch
- python unix_counter.py -p 1500000000 -m 0.01 -o 0
+ python unix_counter.py -p 1500000000
 
 
 COMMAND-LINE EXAMPLE (3):
  F:
  cd F:\\Python\\Projects\\Epoch
- python unix_counter.py --party 1500000000 --moment 0.01 --offset 0
+ python unix_counter.py --party 1500000000
 ---------------------''',
     epilog="""
 ---------------------
@@ -52,20 +52,20 @@ https://www.Twitter.com/VossDataScience
 
 while True:
     try:
-        counter = up.unix_countup(int(sys.argv[1]), float(sys.argv[2]), int(sys.argv[3]),)
+        counter = up.unix_countup(int(sys.argv[1]),)
         counter
     except:
         parser.add_argument('-p', '--party', nargs='*', type=int, default=1500000000, help='The unix time for the party!  Default:[party=1500000000]')
-        parser.add_argument('-m', '--moment', nargs='*', type=float, default=0.01, help='The moment generator for the unix time counter.  Default:[moment=0.01]')
-        parser.add_argument('-o','--offset', nargs='*', type=float, default=0, help='The place out of line from the actual time.  Default:[offset=0]')
+        #parser.add_argument('-m', '--moment', nargs='*', type=float, default=0.01, help='The moment generator for the unix time counter.  Default:[moment=0.01]')
+        #parser.add_argument('-o','--offset', nargs='*', type=float, default=0, help='The place out of line from the actual time.  Default:[offset=0]')
         args=parser.parse_args()
         
         if args.party and args.moment and args.offset:
-            counter = up.unix_countup(args.party[0], args.moment[0], args.offset[0])
+            counter = up.unix_countup(args.party[0])
             counter
         else:
             if __name__ == '__main__':
-                up.unix_countup(1500000000, 0.01, 0)
+                up.unix_countup(1500000000)
 
         #If you want to exit the counter, then press ctrl c
 
